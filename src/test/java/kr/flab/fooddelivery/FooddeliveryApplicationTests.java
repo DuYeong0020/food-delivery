2b6bc72dbd88cbe8f254c67e0cfd63dec34c310d
DuYeong0020 <chlendud00@naver.com>
2022-03-03 15:01:54 +0900

    init

---
+++ src/test/java/kr/flab/fooddelivery/FooddeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.flab.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
